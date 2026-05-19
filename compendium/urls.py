3f14c6399b03d69e0e4f7d8bf01eca0d71c04085
Serhii Hryniuk <serhiibhryniuk@gmail.com>
2026-05-19 20:52:47 +0300

    feat: add skill update page for registered user

--- compendium/urls.py
+++ compendium/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from compendium.views import IndexView, SkillListView, SkillCreateView, SkillDetailView
+from compendium.views import IndexView, SkillListView, SkillCreateView, SkillDetailView, SkillUpdateView
 
 app_name = "compendium"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("skills/", SkillListView.as_view(), name="skill_list"),
     path("skills/<slug:slug>/", SkillDetailView.as_view(), name="skill_detail"),
     path("skills/create/", SkillCreateView.as_view(), name="skill_create"),
+    path("skills/<slug:slug>/update/", SkillUpdateView.as_view(), name="skill_update"),
 ]
--- compendium/views.py
+++ compendium/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, CreateView, DetailView
+from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView
 
 from compendium.models import Skill
 
@@ -29,4 +29,11 @@ class SkillCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class SkillUpdateView(UpdateView):
+    model = Skill
+    fields = ['name', 'slug', 'description', 'dmg_dice', 'skill_type']
+    template_name = 'compendium/skill_form.html'
+    success_url = reverse_lazy('compendium:skill_list')
 
