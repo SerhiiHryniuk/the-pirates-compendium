247285f88d9e341fc82ac06e58a8175fc15f8046
Serhii Hryniuk <serhiibhryniuk@gmail.com>
2026-05-19 18:01:04 +0300

    feat: add skill list page

--- compendium/urls.py
+++ compendium/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from compendium.views import IndexView
+from compendium.views import IndexView, SkillListView
 
 app_name = "compendium"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("skills/", SkillListView.as_view(), name="skill_list"),
 ]
--- compendium/views.py
+++ compendium/views.py
@@ -1,5 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from compendium.models import Skill
+
 
 class IndexView(TemplateView):
     template_name = 'compendium/index.html'
+
+
+class SkillListView(ListView):
+    model = Skill
+    template_name = 'compendium/skill_list.html'
+    context_object_name = 'skill_list'
 
