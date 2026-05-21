b90ee25c00031809605aa150be0c82ea1c74167f
Serhii Hryniuk <serhiibhryniuk@gmail.com>
2026-05-21 23:44:35 +0300

    style: add base template with include templates footer and navbar, redesign index.html

--- compendium/views.py
+++ compendium/views.py
@@ -1,3 +1,5 @@
+from multiprocessing import context
+
 from django import forms
 from django.db.models import F
 from django.db.models.sql import query
@@ -11,6 +13,18 @@ from compendium.models import Skill, DevilFruit, Monster, Scenario
 
 class IndexView(TemplateView):
     template_name = 'compendium/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['skill_count'] = Skill.objects.count()
+        context['devil_fruit_count'] = DevilFruit.objects.count()
+        context['monster_count'] = Monster.objects.count()
+        context['scenario_count'] = Scenario.objects.count()
+        context['top_skills'] = Skill.objects.order_by('-view_count', 'name')[:3]
+        context['top_devil_fruits'] = DevilFruit.objects.order_by('-view_count', 'name')[:3]
+        context['top_monsters'] = Monster.objects.order_by('-view_count', 'name')[:3]
+        context['top_scenarios'] = Scenario.objects.order_by('-view_count', 'title')[:3]
+        return context
 
 
 class SkillListView(ListView):
